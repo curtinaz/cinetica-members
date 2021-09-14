caa3dd2c49f5d70cb843b0ef4c43f5c2225114f4
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-09-14 15:54:41 -0300

    Update aula.php

--- aula.php
+++ aula.php
@@ -4,61 +4,7 @@ require_once 'config.php';
 
 if (!isset($_COOKIE['logado'])) { // testa se o usuário está logado, se não estiver, mostra a tela de Login.
 
-?> 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="style.css">
-    <title>Conecte-se</title>
-</head>
-
-<body>
-    <main>
-        <div class="container">
-        <!-- Login -->
-        <form action="./actions/login.php" class="" id="login-form" method="POST">
-            <h1>Login</h1>
-            <input class="campo" id="mail" name="mail" type="email" placeholder="E-mail" required>
-            <input class="campo" id="senha" name="senha" type="password" placeholder="Senha" required>
-            <input type="submit" value="Entrar">
-
-            <a href="#register" onClick="changeTitle('Registre-se')" id="register_btn" style="margin-bottom: -.4rem;">Não tem uma conta? Cadastre-se</a>
-
-        </form>
-        
-
-        <!-- Cadastro -->
-        <form action="./actions/register.php" class="cadastro off" id="register-form" method="POST" required>
-            <h1>Cadastro</h1>
-            
-            <input class="campo" id="name" name="name" type="text" placeholder="Nome Completo" required>
-
-            <input class="campo" id="telefone" name="telefone" type="tel" placeholder="Número Telefone" required>
-
-            <input class="campo" id="mail" name="mail" type="email" placeholder="E-mail" required>
-
-            <input class="campo" id="senha" name="senha" type="password" placeholder="Senha" required>
-
-            <input type="submit" value="Cadastrar" >
-
-            <a href="#login" onClick="changeTitle('Conecte-se')" id="register_btn2" style="margin-bottom: -.0rem;">Já tem uma conta? Faça o login</a>
-        </form>
-        </div>
-    </main>
-
-
-    <script src="script.js"></script>
-</body>
-
-</html>
-
-<?php
+header('Location: ./');
 
 } else {
     $name = $_COOKIE['name'];
