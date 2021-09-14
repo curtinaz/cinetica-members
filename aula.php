2dcae5891625b7f194ed41c1b282fcbd3a263cc4
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-09-14 19:00:18 -0300

    Update aula.php

--- aula.php
+++ aula.php
@@ -411,7 +411,7 @@ if (!isset($_COOKIE['logado'])) { // testa se o usuário está logado, se não e
 
                         <div class="d-flex align-items-center sidebar-p-a">
                             <img src="./assets/logo.png" width="40%" class="flex align-items-center" style="
-    width: 20%;
+    width: 20%; padding: 8px 40px 0 40px;
 ">
                         </div>
 
